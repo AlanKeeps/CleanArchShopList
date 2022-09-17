f07d19055c63835487f067b7c19f0788125d82d9
AlanKeeps <alan.keeps.walking@gmail.com>
2022-09-18 00:34:46 +0300

    построил базу domain data presentation

---
+++ app/src/main/java/com/example/cleanarchshoplist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.example.cleanarchshoplist.data
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.cleanarchshoplist.domain.ShopItem
+import com.example.cleanarchshoplist.domain.ShopListRepository
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem(name = "name $i", i, true)
+            addShopItem(item)
+        }
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+        updateList()
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+        updateList()
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItemId(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItemId(shopItemId: Int): ShopItem {
+        return shopList.find { it.id == shopItemId }
+            ?: throw RuntimeException("element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
+    }
+}
