3ce925b959823105d0ccdd8a18d997518b2b0b7e
AlanKeeps <alan.keeps.walking@gmail.com>
2022-10-07 21:33:15 +0300

    impl repository

---
+++ app/src/main/java/com/example/cleanarchshoplist/data/ShopListMapper.kt
@@ -0,0 +1,24 @@
+package com.example.cleanarchshoplist.data
+
+import com.example.cleanarchshoplist.domain.ShopItem
+
+class ShopListMapper {
+
+    fun mapEntityToDbModel(shopItem: ShopItem) = ShopItemDbModel(
+        id = shopItem.id,
+        name = shopItem.name,
+        count = shopItem.count,
+        enabled = shopItem.enabled
+    )
+
+    fun mapDbModelToEntity(shopItemDbModel: ShopItemDbModel) = ShopItem(
+        id = shopItemDbModel.id,
+        name = shopItemDbModel.name,
+        count = shopItemDbModel.count,
+        enabled = shopItemDbModel.enabled
+    )
+
+    fun mapDbModelToListEntity(list: List<ShopItemDbModel>) = list.map {
+        mapDbModelToEntity(it)
+    }
+}
