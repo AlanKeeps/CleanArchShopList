9b9a9f9ea0e51885e01aa776bc01c25cdc522708
AlanKeeps <alan.keeps.walking@gmail.com>
2022-10-08 23:20:56 +0300

    use viewModelScope

--- app/src/main/java/com/example/cleanarchshoplist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/cleanarchshoplist/presentation/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.example.cleanarchshoplist.presentation
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cleanarchshoplist.data.ShopListRepositoryImpl
 import com.example.cleanarchshoplist.domain.DeleteShopItemUseCase
 import com.example.cleanarchshoplist.domain.EditShopItemUseCase
@@ -21,25 +22,18 @@ class MainViewModel(application: Application): AndroidViewModel(application) {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    private val scope = CoroutineScope(Dispatchers.IO)
-
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem){
-        scope.launch {
+        viewModelScope.launch {
             deleteShopItemUseCase.deleteShopItem(shopItem)
         }
     }
 
     fun changeEnableState(shopItem: ShopItem){
-        scope.launch {
+        viewModelScope.launch {
             val newItem = shopItem.copy(enabled = !shopItem.enabled)
             editShopItemUseCase.editShopItem(newItem)
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        scope.cancel()
     }
 }
