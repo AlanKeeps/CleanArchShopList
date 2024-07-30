85040063add9c0dfe6b527f35e17b53eacbead3c
Alan <Alan.keeps.walking@gmail.com>
2024-07-30 23:58:02 +0300

    smt

--- app/src/main/java/com/example/cleanarchshoplist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/cleanarchshoplist/presentation/MainViewModel.kt
@@ -2,14 +2,12 @@ package com.example.cleanarchshoplist.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cleanarchshoplist.data.ShopListRepositoryImpl
 import com.example.cleanarchshoplist.domain.DeleteShopItemUseCase
 import com.example.cleanarchshoplist.domain.EditShopItemUseCase
 import com.example.cleanarchshoplist.domain.GetShopListUseCase
 import com.example.cleanarchshoplist.domain.ShopItem
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
@@ -20,25 +18,19 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
 
-    private val scope = CoroutineScope(Dispatchers.IO)
 
     val shopList = getShopListUseCase.getShopList()
 
     fun deleteShopItem(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             deleteShopItemUseCase.deleteShopItem(shopItem)
         }
     }
 
     fun changeEnableState(shopItem: ShopItem) {
-        scope.launch {
+        viewModelScope.launch {
             val newItem = shopItem.copy(enabled = !shopItem.enabled)
             editShopItemUseCase.editShopItem(newItem)
         }
-    }
-
-    override fun onCleared() {
-        super.onCleared()
-        scope.cancel()
     }
 }
